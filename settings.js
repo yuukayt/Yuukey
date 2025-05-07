13918ec46ee003646d7c0688a5fa633a94abb0b1
yuukayt <beesolacag922@gmail.com>
2025-05-07 11:41:08 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qygQFBQB#Vrr1QiTe0w_K8rlIaMElywKl_NfHi61kvwU1eWBFyYk' : process.env.SESSION_ID,
+PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
+SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
+};
